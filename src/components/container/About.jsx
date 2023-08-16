c1367b0e2abf8776816140feea60f68feae8222c
masud rana <masudfcs1@gmail.com>
2023-08-16 10:46:28 +0600

    About

--- src/components/container/About.jsx
+++ src/components/container/About.jsx
@@ -7,6 +7,21 @@ const About = () => {
         <div className=" border-[3px] border-solid border-Teal rounded-lg">
           <img src={about} alt="" className="p-4" />
         </div>
+        <div>
+          <div className=" font-bold sm:text-[1.875rem] text-[1.5rem] mb-5">
+            We provide the <br /> best{" "}
+            <span className=" text-Teal"> online courses</span>
+          </div>
+          <p className=" text-sm text-gray leading-7 mb-4">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+            eum eos nihil repellendus aliquid laudantium fugiat dicta beatae
+            natus alias in at omnis reiciendis, sunt id qui dignissimos non
+            repellat?
+          </p>
+          <button className=" py-3 px-6 text-sm border border-solid border-gray rounded-lg font-bold">
+            Know More{" "}
+          </button>
+        </div>
       </div>
     </div>
   );
